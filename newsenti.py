048be17b569cd921fe990dc6f10c57ecc4363459
Ashish1999Kumar <ashish.k.singh1999@gmail.com>
2019-09-16 22:43:39 +0530

    credentials

--- newsenti.py
+++ newsenti.py
@@ -3,8 +3,8 @@ import pandas as pd
 import json
 import tweepy as tw
 
-CONSUMER_KEY="FnUkoVD3DqObS2DXYnj4v7dLI"
-CONSUMER_SECRET_KEY="mB6VJXOhzNkAgdhbgaScujPPzN2UTnpl0P7Lr3et1K7xgMx2Pm"
+CONSUMER_KEY=""
+CONSUMER_SECRET_KEY=""
 ACCESS_KEY="1152979320459030528-eZoIOHdg1L0DbYRiXb6QFIpXGj5tDU"
 ACCESS_KEY_SECRET="jzkk6RBQxx19B0k0tJ8M2UKAa5zq7OZjLmVj7BSOGJACC"
 
@@ -19,5 +19,5 @@ tweets = tw.Cursor(api.search,q=search_words,lang="en",since=date_since).items(1
 users_locs = [tweet.text for tweet in tweets]
 tweet_text = pd.DataFrame(data=users_locs, 
                     columns=['usertext'])
-export_csv = tweet_text.to_csv (r'/home/ashish/Sentimeter/tee.csv', index = None, header=True) #Don't forget to add '.csv' at the end of the path
+export_csv = tweet_text.to_csv (r'/home/ashish/Sentimeter/tee.csv', index = None, header=True)
 print(tweet_text.head())
