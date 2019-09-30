d906ba5b4ac638d5e80d41e74992b5f2cdf29b56
Ashish1999Kumar <ashish.k.singh1999@gmail.com>
2019-09-30 23:16:54 +0530

    regex

--- newsenti.py
+++ newsenti.py
@@ -1,12 +1,10 @@
 import numpy as np
 import pandas as pd
-import json
+import re
 import tweepy as tw
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-nltk.download('punkt')
-nltk.download('stopwords')
 
 CONSUMER_KEY=""
 CONSUMER_SECRET_KEY=""
@@ -22,9 +20,16 @@ search_words=search_words+" -filter:retweets"
 date_since = "2019-09-15"
 tweets = tw.Cursor(api.search,q=search_words,lang="en",since=date_since).items(10)
 users_locs = [tweet.text for tweet in tweets]
-token_words=[]
 
+user_loc=[]
+email='\shttp\S*'
 for i in users_locs:
+    punc_words=(re.sub('[^A-Za-z\s]+','',i))
+    pattern=re.compile(email)
+    user_loc.append(pattern.sub('',punc_words))
+    
+token_words=[]
+for i in user_loc:
     token_words.append(word_tokenize(i))
     
 stop_words=set(stopwords.words('english'))
@@ -34,8 +39,7 @@ for i in token_words:
     x=[w for w in i if not w in stop_words]
     filtered.append(x)
 
-print(filtered[1])
-tweet_text = pd.DataFrame(data=users_locs, 
+tweet_text = pd.DataFrame(data=user_loc, 
                     columns=['usertext'])
 export_csv = tweet_text.to_csv (r'/home/ashish/Sentimeter/tee.csv', index = None, header=True)
 print(tweet_text.head())
